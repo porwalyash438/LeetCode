d7c0c8bfdf3877560ab866fd9973f3eeae9a624b
porwalyash438 <138603944+porwalyash438@users.noreply.github.com>
2025-03-20 18:55:17 +0530

    Time: 0 ms (100%), Space: 42.4 MB (8.34%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,31 +1,23 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        preorder(root,res);
-        return res;
-    }
-    public void preorder(TreeNode root,List<Integer> res){
-            if(root == null){
-                return ;
-            }
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<TreeNode>();
+        TreeNode node = root;
 
-            preorder(root.left,res);
-            res.add(root.val);
-            preorder(root.right,res);
+        while (true) {
+            if (node != null) {
+                st.push(node);
+                node = node.left;
+            } else {
+                if (st.isEmpty()) {
+                    break;
+                }
+                node = st.pop();
+                list.add(node.val);
+                node = node.right;
+            }
         }
+
+        return list;
     }
+}
