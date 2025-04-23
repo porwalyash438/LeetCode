b66182d786b3fcb9f6284b3442f0b812838ea032
porwalyash438 <138603944+porwalyash438@users.noreply.github.com>
2025-04-23 18:08:12 +0530

    Time: 1 ms (88.91%), Space: 48.2 MB (28.51%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public void dfs(int node, boolean[] visited, int[][] isConnected) {
+        visited[node] = true;
+        for (int i = 0; i < isConnected.length; i++) {
+            if (isConnected[node][i] == 1 && !visited[i]) {
+                dfs(i, visited, isConnected);
+            }
+        }
+    }
+
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        boolean[] visited = new boolean[n];
+        int provinces = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, visited, isConnected);
+                provinces++;
+            }
+        }
+
+        return provinces;
+    }
+}
