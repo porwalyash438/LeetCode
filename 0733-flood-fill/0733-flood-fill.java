f1a5f25e925c0d05cff1de2513b96acf9310366c
porwalyash438 <138603944+porwalyash438@users.noreply.github.com>
2025-10-05 21:04:54 +0530

    Time: 2 ms (8.99%), Space: 45.4 MB (5.53%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,39 +1,52 @@
+class pair{
+    int first;
+    int second;
+    public pair(int first,int second){
+        this.first = first;
+        this.second = second;
+    }
+}
 class Solution {
-
-    private void dfs(int row, int col, int[][] ans, int[][] image, int newColor, int[] delRow, int[] delCol,
-            int initColor) {
-        ans[row][col] = newColor;
+    public void bfs(int [][] image,int [][] ans,int sr,int sc,int color,boolean [][] visited){ 
+        ans[sr][sc] = color;
+        visited[sr][sc] = true;
+        Queue<pair> q = new LinkedList<pair>();
+        q.add(new pair(sr,sc));
         int n = image.length;
         int m = image[0].length;
+        while(!q.isEmpty()){
+            pair p = q.poll();
+            int ro = p.first;
+            int co = p.second;
 
-        for (int i = 0; i < 4; i++) {
-            int nrow = row + delRow[i];
-            int ncol = col + delCol[i];
+            for(int delrow = -1;delrow <=1;delrow++){
+                for(int colrow=-1;colrow<=1;colrow++){
+                    if(Math.abs(delrow) == Math.abs(colrow)) continue;
+                    int nrow = ro + delrow;
+                    int ncol = co + colrow;
 
-            if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m
-                    && image[nrow][ncol] == initColor && ans[nrow][ncol] != newColor) {
-                dfs(nrow, ncol, ans, image, newColor, delRow, delCol, initColor);
+                    if(nrow >=0 && nrow < n && ncol >=0 && ncol <m && ans[nrow][ncol] != color && image[nrow][ncol] == image[ro][co] && !visited[nrow][ncol]){
+                        ans[nrow][ncol] = color;
+                        visited[nrow][ncol] = true;
+
+                        q.add(new pair(nrow,ncol));
+                    }
+                }
             }
         }
     }
-
-    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        int initColor = image[sr][sc];
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int n = image.length;
         int m = image[0].length;
-
-        int[][] ans = new int[n][m];
-        for (int i = 0; i < n; i++) {
+        int currcol = image[sr][sc];
+        int ans[][] = new int [n][m];
+        boolean visited [][] = new boolean[n][m];
+        for(int i=0;i<n;i++){
             ans[i] = image[i].clone();
         }
-
-        int[] delRow = { -1, 0, +1, 0 };
-        int[] delCol = { 0, +1, 0, -1 };
-
-        if (initColor != newColor) {
-            dfs(sr, sc, ans, image, newColor, delRow, delCol, initColor);
+        if(currcol != color){
+            bfs(image,ans,sr,sc,color,visited);
         }
-
         return ans;
     }
 }
